a35e1019fa83f9487d119c3833d93c1bdf0b1377
thispassing <thispassing@users.noreply.github.com>
2018-10-08 11:30:43 -0400

    finished scrabble value of a word

--- NEW.py
+++
@@ -1,5 +0,0 @@
-from pynput.keyboard import Key, Controller
-
-from win32api import GetKeyState 
-from win32con import VK_CAPITAL 
-print(GetKeyState(VK_CAPITAL))
--- labs109.py
+++ labs109.py
@@ -86,7 +86,6 @@ def double_until_all_digits(n, giveup = 1000):
 
 ### cPS LOCK ###
 from pynput.keyboard import Key, Controller
-
 from win32api import GetKeyState 
 from win32con import VK_CAPITAL 
 
@@ -107,6 +106,24 @@ def caps_lock_stuck(text):
 
 
 ### Scrabble value of a word ###
+_scrabble_ = {
+        "a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4,
+        "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3,
+        "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8,
+        "y": 4, "z": 10
+}
 
+def scrabble_value(word, multipliers):
+    sum = 0
+    word = word.lower()
+    if len(word) == len(multipliers):
+        for i, t in zip(word, multipliers):
+            e = int(_scrabble_[i]*t)
+            sum+=e
+    else:
+        print("Invalid scrabble value.\nScore:")
+    print(sum)
+
+#scrabble_value("world", [1,3,1,1,1])
 
         
