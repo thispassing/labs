5bee2640bb89d4b12e05768ad9c31b6065c6570a
thispassing <thispassing@users.noreply.github.com>
2018-10-09 19:54:07 -0400

    finished detab problem

--- labs109.py
+++ labs109.py
@@ -3,34 +3,34 @@
 
 ### Ryerson letter grade ###
 def ryerson_letter_grade(pct):
-    if pct < 50:
-        return 'F'
-    elif 50 <= pct <=52:
-        return 'D-'
-    elif 53 <= pct <=56:
-        return 'D'
-    elif 57 <= pct <=59:
-        return 'D+'
-    elif 60 <= pct <=62:
-        return 'C-'
-    elif 63 <= pct <=66:
-        return 'C'
-    elif 67 <= pct <=69:
-        return 'C+'
-    elif 70 <= pct <=72:
-        return 'B-'
-    elif 73 <= pct <=76:
-        return 'B'
-    elif 77 <= pct <=79:
-        return 'B+'
-    elif 80 <= pct <=84:
-        return 'A-'
-    elif 85 <= pct <=89:
-        return 'A'
-    elif 90 <= pct <=120:
-        return 'A+'
-    else:
-        return "This isn't a valid grade, or you cheated!"
+	if pct < 50:
+		return 'F'
+	elif 50 <= pct <=52:
+		return 'D-'
+	elif 53 <= pct <=56:
+		return 'D'
+	elif 57 <= pct <=59:
+		return 'D+'
+	elif 60 <= pct <=62:
+		return 'C-'
+	elif 63 <= pct <=66:
+		return 'C'
+	elif 67 <= pct <=69:
+		return 'C+'
+	elif 70 <= pct <=72:
+		return 'B-'
+	elif 73 <= pct <=76:
+		return 'B'
+	elif 77 <= pct <=79:
+		return 'B+'
+	elif 80 <= pct <=84:
+		return 'A-'
+	elif 85 <= pct <=89:
+		return 'A'
+	elif 90 <= pct <=120:
+		return 'A+'
+	else:
+		return "This isn't a valid grade, or you cheated!"
 
 #grade = int(input("enter grade: "))
 #print(ryerson_letter_grade(grade))
@@ -38,19 +38,19 @@ def ryerson_letter_grade(pct):
 
 ### Ascending list ###
 def is_ascending(items):
-    this = True
-    for i in range(len(items)):
-        if i==0:
-            continue
-        elif items[i] <= items[i-1]:
-            this = False
-            break
-        elif items[i] >= items[i-1]:
-            continue
-    if this:
-        print("True")
-    else:
-        print("False")
+	this = True
+	for i in range(len(items)):
+		if i==0:
+			continue
+		elif items[i] <= items[i-1]:
+			this = False
+			break
+		elif items[i] >= items[i-1]:
+			continue
+	if this:
+		print("True")
+	else:
+		print("False")
 
 #is_ascending([5,91,90,773,3454])
 
@@ -70,16 +70,16 @@ arr = [zero, one, two, three, four, five, six, seven, eight, nine]
 
 
 def double_until_all_digits(n, giveup = 1000):
-    attempts = 0
-    while attempts != giveup:
-        if all(x in str(n) for x in arr) == True:
-            break
-        n=n*2
-        attempts = attempts + 1
-    if attempts == giveup:
-        print("-1")
-    else:
-        print(attempts)
+	attempts = 0
+	while attempts != giveup:
+		if all(x in str(n) for x in arr) == True:
+			break
+		n=n*2
+		attempts = attempts + 1
+	if attempts == giveup:
+		print("-1")
+	else:
+		print(attempts)
 
 #double_until_all_digits(1)  # answer should be 68
 
@@ -93,36 +93,36 @@ keyboard = Controller()
 this = 'bcdefghijklmnopqrstuvwxyzBCDEFGHIJKLMNOPQRSTUVWXYZ '
 
 def caps_lock_stuck(text):
-    if GetKeyState(VK_CAPITAL) == 1:
-        keyboard.press(Key.caps_lock)
-    for i in text:
-        if i in this:
-            keyboard.type(i)
-        else:
-            keyboard.press(Key.caps_lock)
-            keyboard.release(Key.caps_lock)
-     
+	if GetKeyState(VK_CAPITAL) == 1:
+		keyboard.press(Key.caps_lock)
+	for i in text:
+		if i in this:
+			keyboard.type(i)
+		else:
+			keyboard.press(Key.caps_lock)
+			keyboard.release(Key.caps_lock)
+	 
 #caps_lock_stuck("Madder than Mad Brian of Madcastle")
 
 
 ### Scrabble value of a word ###
 _scrabble_ = {
-        "a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4,
-        "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3,
-        "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8,
-        "y": 4, "z": 10
+		"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4,
+		"i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3,
+		"q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8,
+		"y": 4, "z": 10
 }
 
 def scrabble_value(word, multipliers):
-    sum = 0
-    word = word.lower()
-    if len(word) == len(multipliers):
-        for i, t in zip(word, multipliers):
-            e = int(_scrabble_[i]*t)
-            sum+=e
-    else:
-        print("Invalid scrabble value.\nScore:")
-    print(sum)
+	sum = 0
+	word = word.lower()
+	if len(word) == len(multipliers):
+		for i, t in zip(word, multipliers):
+			e = int(_scrabble_[i]*t)
+			sum+=e
+	else:
+		print("Invalid scrabble value.\nScore:")
+	print(sum)
 
 #scrabble_value("hello", [1,1,1,1,1])
 
@@ -131,66 +131,66 @@ def scrabble_value(word, multipliers):
 import numpy as np
 
 def create_zigzag(rows, cols, start = 1):
-    full = []
-    digits = start + rows * cols
-    
-    output = np.arange(start,digits,1).reshape(rows,cols)
-    #print(output)
+	full = []
+	digits = start + rows * cols
+	
+	output = np.arange(start,digits,1).reshape(rows,cols)
+	#print(output)
 
-    for i,l in zip(output,range(len(output))):
-        if l & 1:
-            i = list(i)
-            i.reverse()
-            full.append(i)
-        else:
-            i = list(i)
-            full.append(i)
-    print(full)
-        
+	for i,l in zip(output,range(len(output))):
+		if l & 1:
+			i = list(i)
+			i.reverse()
+			full.append(i)
+		else:
+			i = list(i)
+			full.append(i)
+	print(full)
+		
 #create_zigzag(3, 5)
 
 ### Bingo bango bongo, I don't want to leave the Python ###
 
 def contains_bingo(card, numbers, centerfree = False):
-    cf = centerfree
-    cfn = (card[2][2])
-    checklist = [
-    [card[0][0], card[1][1], cfn, card[3][3], card[4][4]],
-    (card[4][0], card[3][1], cfn, card[1][3], card[0][4]),
-    (card[0][0], card[0][1], card[0][2], card[0][3], card[0][4]),
-    (card[1][0], card[1][1], card[1][2], card[1][3], card[1][4]),
-    (card[2][0], card[2][1], cfn, card[2][3], card[2][4]),
-    (card[3][0], card[3][1], card[3][2], card[3][3], card[3][4]),
-    (card[4][0], card[4][1], card[4][2], card[4][3], card[4][4]),
-    (card[0][0], card[1][0], card[2][0], card[3][0], card[4][0]),
-    (card[0][1], card[1][1], card[2][1], card[3][1], card[4][1]),
-    (card[0][2], card[1][2], cfn, card[3][2], card[4][2]),
-    (card[0][3], card[1][3], card[2][3], card[3][3], card[4][3]),
-    (card[0][4], card[1][4], card[2][4], card[3][4], card[4][4])
-    ]
-    for line in checklist:
-        sumsum = 0
-        for num in line:
-            if num in numbers:
-                num = 0
-                sumsum+=num
-            elif num == cfn and cf == True:
-                num = 0
-                sumsum+=num
-            elif num == cfn and cf == False:
-                num = 1
-                sumsum+=num
-            else:
-                num = 1
-                sumsum+=num
-        if sumsum == 0:
-            result = "True"
-            break
-        elif sumsum > 0:
-            result = "False"
-            continue
-    #print(result)
-    #print(sumsum)
+	cf = centerfree
+	cfn = (card[2][2])
+	checklist = [
+	[card[0][0], card[1][1], cfn, card[3][3], card[4][4]],
+	(card[4][0], card[3][1], cfn, card[1][3], card[0][4]),
+	(card[0][0], card[0][1], card[0][2], card[0][3], card[0][4]),
+	(card[1][0], card[1][1], card[1][2], card[1][3], card[1][4]),
+	(card[2][0], card[2][1], cfn, card[2][3], card[2][4]),
+	(card[3][0], card[3][1], card[3][2], card[3][3], card[3][4]),
+	(card[4][0], card[4][1], card[4][2], card[4][3], card[4][4]),
+	(card[0][0], card[1][0], card[2][0], card[3][0], card[4][0]),
+	(card[0][1], card[1][1], card[2][1], card[3][1], card[4][1]),
+	(card[0][2], card[1][2], cfn, card[3][2], card[4][2]),
+	(card[0][3], card[1][3], card[2][3], card[3][3], card[4][3]),
+	(card[0][4], card[1][4], card[2][4], card[3][4], card[4][4])
+	]
+	for line in checklist:
+		sumsum = 0
+		for num in line:
+			if num in numbers:
+				num = 0
+				sumsum+=num
+			elif num == cfn and cf == True:
+				num = 0
+				sumsum+=num
+			elif num == cfn and cf == False:
+				num = 1
+				sumsum+=num
+			else:
+				num = 1
+				sumsum+=num
+		if sumsum == 0:
+			result = "True"
+			break
+		elif sumsum > 0:
+			result = "False"
+			continue
+	#print(result)
+	#print(sumsum)
 
 contains_bingo([
 [89, 23, 61, 94, 67], 
@@ -207,27 +207,27 @@ contains_bingo([
 ### Group equal consecutive elements into sublists ###
 result = []
 def group_equal(items):
-    new = []
-    for i,n in zip(items,range(len(items))):
-        if new == []:
-            if items[-1] == i:    
-                if i == items[(n-1)]:
-                    new.insert(len(new),i)
-                elif i!= items[(n-1)]:
-                    result.insert(len(result),new)
-                    new = []
-                    new.insert(len(new),i)
-            else:
-                new.insert(len(new),i)
-        elif new != []:
-            if i == items[(n-1)]:
-                new.insert(len(new),i)
-            elif i!= items[(n-1)]:
-                result.insert(len(result),new)
-                new = []
-                new.insert(len(new),i)
-    result.insert(len(result),new)
-    print(result)
+	new = []
+	for i,n in zip(items,range(len(items))):
+		if new == []:
+			if items[-1] == i:    
+				if i == items[(n-1)]:
+					new.insert(len(new),i)
+				elif i!= items[(n-1)]:
+					result.insert(len(result),new)
+					new = []
+					new.insert(len(new),i)
+			else:
+				new.insert(len(new),i)
+		elif new != []:
+			if i == items[(n-1)]:
+				new.insert(len(new),i)
+			elif i!= items[(n-1)]:
+				result.insert(len(result),new)
+				new = []
+				new.insert(len(new),i)
+	result.insert(len(result),new)
+	print(result)
 
 #group_equal([1, 1, 4, 4, 4, "hello", "hello", 4])
 
@@ -241,15 +241,43 @@ def group_equal(items):
 import statistics as st
 
 def running_median_of_three(items):
-    new = []
-    for i,n in zip(items,range(len(items))):
-        if i == items[0]:
-            new.insert(len(new),i)
-        elif i == items[1]:
-            new.insert(len(new),i)
-        else:
-            med = st.median([items[(n-2)], items[(n-1)], items[(n)]])
-            new.insert(len(new),med)
-    print(new)
+	new = []
+	for i,n in zip(items,range(len(items))):
+		if i == items[0]:
+			new.insert(len(new),i)
+		elif i == items[1]:
+			new.insert(len(new),i)
+		else:
+			med = st.median([items[(n-2)], items[(n-1)], items[(n)]])
+			new.insert(len(new),med)
+	print(new)
 
 #running_median_of_three([5, 2, 9, 1, 7, 4, 6, 3, 8])
+
+### Deteb ###
+def detab(string, n = 5, sound = "+"):
+    result = ""
+    pos = 0
+    yes = (string[(n-1)::n])
+    #print(yes)
+    for char,num in zip(string,range(len(string))):
+        if char == "\t":
+            if string[(num+1)] in yes:
+                char = ""
+                # instead of the tab character, append the
+                # number of spaces to the next tab stop
+            else:
+                char = sound * (n - pos % n)
+                pos = 0
+            #if num in (string[::n]) == True:
+                #print('ya')
+        elif char == "\n":
+            pos = 0
+        else:
+            pos += 1
+        result += char
+    print(result)
+
+#detab("Hello\tthereyou\tworld")
+#detab("Ilkka\tMarkus\tKokkarinen")
+#detab("Tenser,\tsaid\tthe\ttensor")
